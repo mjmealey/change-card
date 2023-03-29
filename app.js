2ccfac313275f8515619842b14a0aa186bf92a55
mjmealey <123016396+mjmealey@users.noreply.github.com>
2023-03-29 13:20:08 -0500

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const back = document.getElementById("back")
-
-back.addEventListener("click", (e) => {
-    if(e.target.id === "back"){
-        window.location.href="https://mjmealey.github.io/central-page/"
-    }
-})
